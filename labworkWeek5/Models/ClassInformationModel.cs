9b5282819b1380e0cae07315472cfe9c2e8230c8
Batuhan Karaköy <bkarakoy0@gmail.com>
2025-03-28 18:20:04 +0300

    class information page and actions(delete, edit) added

--- labworkWeek5/Models/ClassInformationModel.cs
+++ labworkWeek5/Models/ClassInformationModel.cs
@@ -1,40 +1,15 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using labworkWeek5.Models;
-using System.Collections.Generic;
-using System.Linq;
+using System.ComponentModel.DataAnnotations;
 
-namespace labworkWeek5.Pages
-{
-    public class IndexModel : PageModel
-    {
-        // Static list to store class information
-        public static List<ClassInformationModel> ClassList { get; set; } = new List<ClassInformationModel>();
+namespace labworkWeek5.Models {
+    public class ClassInformationModel {
+        public int Id { get; set; }
 
-        [BindProperty]
-        public ClassInformationModel NewClass { get; set; } = new ClassInformationModel();
+        [Required]
+        public string ClassName { get; set; }
 
-        public void OnGet() { }
+        [Required]
+        public int StudentCount { get; set; }
 
-        public IActionResult OnPostAdd()
-        {
-            if (!ModelState.IsValid)
-                return Page();
-
-            // Accessing ClassList with the class name because it is static
-            IndexModel.ClassList.Add(NewClass);
-            return RedirectToPage();
-        }
-
-        public IActionResult OnPostDelete(int id)
-        {
-            // Accessing ClassList with the class name because it is static
-            var item = IndexModel.ClassList.FirstOrDefault(x => x.Id == id);
-            if (item != null)
-            {
-                IndexModel.ClassList.Remove(item);
-            }
-            return RedirectToPage();
-        }
+        public string Description { get; set; }
     }
 }
--- labworkWeek5/Pages/Index.cshtml.cs
+++ labworkWeek5/Pages/Index.cshtml.cs
@@ -1,24 +1,63 @@
-using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using labworkWeek5.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace labworkWeek5.Models
-{
-    public class ClassInformationModel
-    {
-        private static int counter = 1;
+namespace labworkWeek5.Pages {
+    public class IndexModel : PageModel {
+        [BindProperty]
+        public ClassInformationModel NewClass { get; set; } = new ClassInformationModel();
 
-        public ClassInformationModel()
-        {
-            Id = counter++;
+        public static List<ClassInformationModel> ClassList { get; set; } = new List<ClassInformationModel>();
+
+        private static int _idCounter = 1;
+
+        public void OnGet() {
+            NewClass = new ClassInformationModel();
         }
 
-        public int Id { get; private set; }
+        public IActionResult OnPostAdd() {
+            if (!ModelState.IsValid)
+                return Page();
 
-        [Required]
-        public string ClassName { get; set; }
+            NewClass.Id = _idCounter++;
+            ClassList.Add(NewClass);
+            NewClass = new ClassInformationModel();
 
-        [Required]
-        public int StudentCount { get; set; }
+            return RedirectToPage();
+        }
 
-        public string Description { get; set; }
+        public IActionResult OnPostDelete(int id) {
+            var item = ClassList.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+                ClassList.Remove(item);
+
+            return RedirectToPage();
+        }
+
+        public IActionResult OnPostEdit(int id) {
+            var item = ClassList.FirstOrDefault(x => x.Id == id);
+            if (item != null) {
+                NewClass = new ClassInformationModel {
+                    Id = item.Id,
+                    ClassName = item.ClassName,
+                    StudentCount = item.StudentCount,
+                    Description = item.Description
+                };
+            }
+            return Page();
+        }
+
+        public IActionResult OnPostUpdate() {
+            var existing = ClassList.FirstOrDefault(x => x.Id == NewClass.Id);
+            if (existing != null) {
+                existing.ClassName = NewClass.ClassName;
+                existing.StudentCount = NewClass.StudentCount;
+                existing.Description = NewClass.Description;
+            }
+            NewClass = new ClassInformationModel();
+            return RedirectToPage();
+        }
     }
 }
